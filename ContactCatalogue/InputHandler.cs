ae2f1b899faa7807bcc2fd91f58e8a34341a13a6
Liza Hjortling <lizhj90@outlook.com>
2025-10-07 13:36:16 +0200

    created class for input

---
+++ ContactCatalogue/InputHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactCatalogue
+{
+    public static class InputHandler
+    {
+
+        public static int GetId()
+        {
+            while (true)
+            {
+
+                Console.WriteLine("Skriv in ett Id:");
+                var idInput = Console.ReadLine();
+                if (int.TryParse(idInput, out int id))
+                {
+                    return id;
+                }
+                else
+                {
+                    Console.WriteLine($"Ogiltigt Id ({idInput}) endast siffror, prova igen.");
+                }
+
+            }
+
+        }
+        public static string GetEmail()
+        {
+
+            while (true)
+            {
+
+                try
+                {
+
+                    Console.WriteLine("Skriv in EmailAdress:");
+                    var emailInput = Console.ReadLine();
+                    var email = new System.Net.Mail.MailAddress(emailInput!); // the constuctor throws an exception if the email is invalid.
+                    return email.Address;
+                }
+                catch { Console.WriteLine("Fel inmatning av email, prova igen"); }
+
+
+            }
+
+
+        }
+    }
+}
