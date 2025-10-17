758ef476ebde1d4248c677eb6d182428ba0c8023
Liza Hjortling <lizhj90@outlook.com>
2025-10-17 09:42:12 +0200

    implement logger in Catalog/Inputhandler

--- ContactCatalogue/Program.cs
+++ ContactCatalogue/Program.cs
@@ -1,17 +1,21 @@
-﻿namespace ContactCatalogue
+﻿using Microsoft.Extensions.Logging;
+
+
+namespace ContactCatalogue
 {
     public class Program
     {
         static void Main(string[] args)
         {
+            using var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+            ILogger logger = loggerFactory.CreateLogger<Program>();
 
-            var menu = new Menu(new Catalog());
+            InputHandler.Logger = logger;
+            var menu = new Menu(new Catalog(logger));
             menu.ShowMenu();
 
 
-            // fixa exportering från CSV
-            // validera email, fixa så du inte kan skriva samma mail med lite bokstav
-            // Loggning via Microsoft.Extensions.Logging (ConsoleLogger).
+         
 
 
         }
